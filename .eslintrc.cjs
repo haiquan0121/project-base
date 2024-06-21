ba37fb82f50ff414ff111b438e1eb8638cdc9836
haiquan0121 <quannh@phx-smartschool.com>
2024-06-21 08:31:25 +0700

    feat: add code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,56 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	settings: {
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true,
+				project: './tsconfig.json'
+			}
+		}
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'eslint-config-prettier',
+		'prettier'
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'tsconfig.node.json', 'tsconfig.json', 'node_modules'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'prettier'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'import/no-unresolved': ['error'],
+		'@typescript-eslint/no-explicit-any': 'off',
+		'react-refresh/only-export-components': 'off',
+		// 'no-unused-vars': ['off', { varsIgnorePattern: '^_$', argsIgnorePattern: '^_$' }],
+		// '@typescript-eslint/no-unused-vars': [
+		// 	'off',
+		// 	{
+		// 		varsIgnorePattern: '^_$',
+		// 		argsIgnorePattern: '^_$'
+		// 	}
+		// ],
+		// '@typescript-eslint/no-unused-vars': 'warn',
+		'no-console': 'error',
+		'import/no-named-as-default-member': 'off',
+		'prettier/prettier': [
+			'warn',
+			{
+				semi: true,
+				tabWidth: 2,
+				useTabs: true,
+				singleQuote: true,
+				printWidth: 120,
+				jsxSingleQuote: true,
+				endOfLine: 'auto',
+				trailingComma: 'none',
+				arrowParens: 'always'
+			}
+		]
+	}
+};
